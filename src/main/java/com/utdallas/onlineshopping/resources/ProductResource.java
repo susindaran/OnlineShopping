e9511c0876af3f4571f77c30a60ee167177783a5
prathyushark <prathyusha.kanmanthreddy@gmail.com>
2017-02-24 20:10:49 -0600

    added delete product functionality

--- src/main/java/com/utdallas/onlineshopping/resources/ProductResource.java
+++ src/main/java/com/utdallas/onlineshopping/resources/ProductResource.java
@@ -4,15 +4,14 @@ import com.codahale.metrics.annotation.Timed;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.utdallas.onlineshopping.action.product.AddProductAction;
+import com.utdallas.onlineshopping.action.product.DeleteProductAction;
 import com.utdallas.onlineshopping.payload.request.product.AddProductRequest;
 import com.utdallas.onlineshopping.payload.response.product.ProductResponse;
 import io.dropwizard.hibernate.UnitOfWork;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -24,11 +23,14 @@ import javax.ws.rs.core.Response;
 public class ProductResource
 {
     private final AddProductAction addProductAction;
+    private final DeleteProductAction deleteProductAction;
 
     @Inject
-    public ProductResource(Provider<AddProductAction> addProductActionProvider)
+    public ProductResource(Provider<AddProductAction> addProductActionProvider,
+                           Provider<DeleteProductAction> deleteProductActionProvider)
     {
         this.addProductAction = addProductActionProvider.get();
+        this.deleteProductAction = deleteProductActionProvider.get();
     }
 
     @POST
@@ -39,4 +41,15 @@ public class ProductResource
         ProductResponse productResponse = addProductAction.withRequest(addProductRequest).invoke();
         return Response.status(Response.Status.CREATED).entity(productResponse).build();
     }
+
+    @DELETE
+    @Path("/{id}")
+    @UnitOfWork
+    @Timed
+    public Response delete(@Context HttpHeaders headers, @NotNull @PathParam("id") String productId)
+    {
+        this.deleteProductAction.withProductId(productId).invoke();
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
 }

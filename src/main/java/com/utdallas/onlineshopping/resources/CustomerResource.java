3dff21d99ac2f2353e736d33d754481ef0a2acdf
Susindaran Elangovan <susindaran94@gmail.com>
2017-02-24 18:22:33 -0600

    Update address and card_detail apis

--- src/main/java/com/utdallas/onlineshopping/resources/CustomerResource.java
+++ src/main/java/com/utdallas/onlineshopping/resources/CustomerResource.java
@@ -5,14 +5,16 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.utdallas.onlineshopping.action.address.AddAddressAction;
 import com.utdallas.onlineshopping.action.address.DeleteAddressAction;
+import com.utdallas.onlineshopping.action.address.EditAddressAction;
 import com.utdallas.onlineshopping.action.card.AddCardDetailAction;
 import com.utdallas.onlineshopping.action.card.DeleteCardDetailAction;
+import com.utdallas.onlineshopping.action.card.EditCardDetailAction;
 import com.utdallas.onlineshopping.action.customer.ChallengeLoginAction;
 import com.utdallas.onlineshopping.action.customer.CreateCustomerAction;
 import com.utdallas.onlineshopping.action.customer.GetCustomerAction;
 import com.utdallas.onlineshopping.action.customer.UpdateCustomerAction;
-import com.utdallas.onlineshopping.payload.request.address.AddAddressRequest;
-import com.utdallas.onlineshopping.payload.request.card.AddCardDetailRequest;
+import com.utdallas.onlineshopping.payload.request.address.AddressRequest;
+import com.utdallas.onlineshopping.payload.request.card.CardDetailRequest;
 import com.utdallas.onlineshopping.payload.request.customer.ChallengeLoginRequest;
 import com.utdallas.onlineshopping.payload.request.customer.CreateCustomerRequest;
 import com.utdallas.onlineshopping.payload.request.customer.UpdateCustomerRequest;
@@ -43,6 +45,8 @@ public class CustomerResource
     private final DeleteAddressAction deleteAddressAction;
     private final AddCardDetailAction addCardDetailAction;
     private final DeleteCardDetailAction deleteCardDetailAction;
+    private final EditAddressAction editAddressAction;
+    private final EditCardDetailAction editCardDetailAction;
 
     @Inject
     public CustomerResource(Provider<GetCustomerAction> getCustomerActionProvider,
@@ -52,7 +56,9 @@ public class CustomerResource
                             Provider<AddAddressAction> addAddressActionProvider,
                             Provider<AddCardDetailAction> addCardDetailActionProvider,
                             Provider<DeleteAddressAction> deleteAddressActionProvider,
-                            Provider<DeleteCardDetailAction> deleteCardDetailActionProvider)
+                            Provider<DeleteCardDetailAction> deleteCardDetailActionProvider,
+                            Provider<EditAddressAction> editAddressActionProvider,
+                            Provider<EditCardDetailAction> editCardDetailActionProvider)
     {
         this.getCustomerAction = getCustomerActionProvider.get();
         this.createCustomerAction = createCustomerActionProvider.get();
@@ -62,6 +68,8 @@ public class CustomerResource
         this.deleteAddressAction = deleteAddressActionProvider.get();
         this.addCardDetailAction = addCardDetailActionProvider.get();
         this.deleteCardDetailAction = deleteCardDetailActionProvider.get();
+        this.editAddressAction = editAddressActionProvider.get();
+        this.editCardDetailAction = editCardDetailActionProvider.get();
     }
 
     @GET
@@ -111,9 +119,9 @@ public class CustomerResource
     @Path("/address/{customer_id}")
     @UnitOfWork
     @Timed
-    public Response addAddress(@Context HttpHeaders headers, @NotNull AddAddressRequest addAddressRequest, @NotNull @PathParam("customer_id") Long customerId)
+    public Response addAddress(@Context HttpHeaders headers, @NotNull AddressRequest addressRequest, @NotNull @PathParam("customer_id") Long customerId)
     {
-        AddressResponse addressResponse = this.addAddressAction.withRequest(addAddressRequest).forCustomerId(customerId).invoke();
+        AddressResponse addressResponse = this.addAddressAction.withRequest(addressRequest).forCustomerId(customerId).invoke();
         return Response.status(Response.Status.CREATED).entity(addressResponse).build();
     }
 
@@ -127,6 +135,18 @@ public class CustomerResource
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
+
+    @PUT
+    @Path("/address/{address_id}")
+    @UnitOfWork
+    @Timed
+    public Response editAddress(@Context HttpHeaders headers, @NotNull AddressRequest addressRequest, @NotNull @PathParam("address_id") Long addressId)
+    {
+        AddressResponse addressResponse = this.editAddressAction.forAddressId(addressId).withRequest(addressRequest).invoke();
+        return Response.status(Response.Status.OK).entity( addressResponse ).build();
+    }
+
+
     /*
     * Card APIs
     * */
@@ -135,9 +155,9 @@ public class CustomerResource
     @Path("/card/{customer_id}")
     @UnitOfWork
     @Timed
-    public Response addCardDetail(@Context HttpHeaders headers, @NotNull AddCardDetailRequest addCardDetailRequest, @NotNull @PathParam("customer_id") Long customerId)
+    public Response addCardDetail(@Context HttpHeaders headers, @NotNull CardDetailRequest cardDetailRequest, @NotNull @PathParam("customer_id") Long customerId)
     {
-        CardDetailResponse cardDetailResponse = this.addCardDetailAction.withRequest(addCardDetailRequest).forCustomerId(customerId).invoke();
+        CardDetailResponse cardDetailResponse = this.addCardDetailAction.withRequest(cardDetailRequest).forCustomerId(customerId).invoke();
         return Response.status(Response.Status.CREATED).entity(cardDetailResponse).build();
     }
 
@@ -149,5 +169,15 @@ public class CustomerResource
     {
         this.deleteCardDetailAction.withCardNumber( cardNumber ).invoke();
         return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    @PUT
+    @Path("/card/{card_number}")
+    @UnitOfWork
+    @Timed
+    public Response editCardDetail(@Context HttpHeaders headers,@NotNull CardDetailRequest cardDetailRequest, @NotNull @PathParam("card_number") String cardNumber)
+    {
+        CardDetailResponse cardDetailResponse = this.editCardDetailAction.forCardNumber(cardNumber).withRequest(cardDetailRequest).invoke();
+        return Response.status(Response.Status.OK).entity( cardDetailResponse ).build();
     }
 }

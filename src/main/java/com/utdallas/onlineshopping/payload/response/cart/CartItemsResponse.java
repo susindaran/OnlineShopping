68a24a78615623c4b3f57d56172c03387f8c4f5b
Susindaran Elangovan <susindaran94@gmail.com>
2017-03-18 21:17:55 -0500

    Added get items in cart API

---
+++ src/main/java/com/utdallas/onlineshopping/payload/response/cart/CartItemsResponse.java
@@ -0,0 +1,19 @@
+package com.utdallas.onlineshopping.payload.response.cart;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.utdallas.onlineshopping.payload.response.customer.CustomerResponse;
+import io.dropwizard.jackson.JsonSnakeCase;
+import lombok.Data;
+
+import java.util.List;
+
+@JsonSnakeCase
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CartItemsResponse
+{
+    @JsonIgnoreProperties(value = {"addresses", "card_details"})
+    private CustomerResponse customer;
+    private List<CartResponse> cartItems;
+}
--- src/main/java/com/utdallas/onlineshopping/resources/CartResource.java
+++ src/main/java/com/utdallas/onlineshopping/resources/CartResource.java
@@ -4,15 +4,15 @@ import com.codahale.metrics.annotation.Timed;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.utdallas.onlineshopping.action.cart.AddProductToCartAction;
+import com.utdallas.onlineshopping.action.cart.GetItemsInCartAction;
 import com.utdallas.onlineshopping.payload.request.cart.AddProductToCartRequest;
+import com.utdallas.onlineshopping.payload.response.cart.CartItemsResponse;
 import com.utdallas.onlineshopping.payload.response.cart.CartResponse;
 import io.dropwizard.hibernate.UnitOfWork;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -24,11 +24,14 @@ import javax.ws.rs.core.Response;
 public class CartResource
 {
     private final AddProductToCartAction addProductToCartAction;
+    private final GetItemsInCartAction getItemsInCartAction;
 
     @Inject
-    public CartResource(Provider<AddProductToCartAction> addProductToCartActionProvider)
+    public CartResource(Provider<AddProductToCartAction> addProductToCartActionProvider,
+                        Provider<GetItemsInCartAction> getItemsInCartActionProvider)
     {
         this.addProductToCartAction = addProductToCartActionProvider.get();
+        this.getItemsInCartAction = getItemsInCartActionProvider.get();
     }
 
     @POST
@@ -38,5 +41,15 @@ public class CartResource
     {
         CartResponse cartResponse = this.addProductToCartAction.withRequest(request).invoke();
         return Response.status( Response.Status.CREATED ).entity( cartResponse ).build();
+    }
+
+    @GET
+    @Path("/{customer_id}")
+    @UnitOfWork
+    @Timed
+    public Response getCartItem(@Context HttpHeaders headers, @NotNull @PathParam("customer_id") Long customerId)
+    {
+        CartItemsResponse cartItemsResponse = this.getItemsInCartAction.forCustomerId(customerId).invoke();
+        return Response.status( Response.Status.OK ).entity( cartItemsResponse ).build();
     }
 }

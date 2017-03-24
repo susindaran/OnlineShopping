d562ce1c9ff0941c5ff47c8805a99348c8e3991e
Susindaran Elangovan <susindaran94@gmail.com>
2017-03-23 22:01:48 -0500

    Removing unused imports

--- src/main/java/com/utdallas/onlineshopping/payload/response/orderdetail/OrderDetailResponse.java
+++ src/main/java/com/utdallas/onlineshopping/payload/response/orderdetail/OrderDetailResponse.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.utdallas.onlineshopping.enumerations.OrderStatus;
 import com.utdallas.onlineshopping.payload.response.product.ProductResponse;
-import com.utdallas.onlineshopping.payload.response.shipment.ShipmentResponse;
 import io.dropwizard.jackson.JsonSnakeCase;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Getter;
 
 @JsonSnakeCase
 @Data

5c9aaf3671ed98a88400a343bcfb1a62e2ee1198
Susindaran Elangovan <susindaran94@gmail.com>
2017-09-27 03:05:31 -0500

    Getting mysql creds from env variables

--- src/main/java/com/betadevels/onlineshopping/configurations/OnlineShoppingConfiguration.java
+++ src/main/java/com/betadevels/onlineshopping/configurations/OnlineShoppingConfiguration.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class OnlineShoppingConfiguration extends Configuration
 {
@@ -18,7 +20,11 @@ public class OnlineShoppingConfiguration extends Configuration
     @Valid
     @NotNull
     @JsonProperty("databaseConfiguration")
-    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+    private DataSourceFactory dataSourceFactory = getDS();
+
+    public OnlineShoppingConfiguration() throws URISyntaxException
+    {
+    }
 
     public DataSourceFactory getDatabaseConfiguration()
     {
@@ -31,4 +37,21 @@ public class OnlineShoppingConfiguration extends Configuration
 
     @JsonProperty("swagger")
     public SwaggerBundleConfiguration swaggerBundleConfiguration;
+
+    public static DataSourceFactory getDS() throws URISyntaxException
+    {
+        URI jdbUri = new URI(System.getenv("JAWSDB_URL" ));
+
+        String username = jdbUri.getUserInfo().split(":")[0];
+        String password = jdbUri.getUserInfo().split(":")[1];
+        String port = String.valueOf(jdbUri.getPort());
+        String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+
+        DataSourceFactory fac = new DataSourceFactory();
+        fac.setUrl( jdbUrl );
+        fac.setUser( username );
+        fac.setPassword( password );
+
+        return fac;
+    }
 }
